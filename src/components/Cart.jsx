039a14b2da9136fdc6d70b51f5dfc19fad7f4b18
Arun <69082994+Arun9600@users.noreply.github.com>
2024-01-02 23:48:06 +0530

    add to cart product quantity added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,7 @@
 import { Box, Drawer, Typography, Grid } from "@mui/material";
 const Cart = ({ cartArea, setCartArea, cart }) => {
+  const subTotal =
+    cart && cart.reduce((acc, item) => acc + item.quantity * item.price, 0);
   return (
     <Box>
       <Drawer
@@ -8,45 +10,109 @@ const Cart = ({ cartArea, setCartArea, cart }) => {
         onClose={() => setCartArea(false)}
         PaperProps={{ sx: { width: "500px" } }}
       >
-        <Box sx={{ padding: "30px" }}>
+        <Box sx={{ padding: "30px 0px" }}>
           <Typography
             variant="h3"
-            sx={{ fontSize: "18px", fontWeight: "bold" }}
+            sx={{
+              fontSize: "18px",
+              fontWeight: "bold",
+              textIndent: "25px",
+              marginBottom: "20px",
+            }}
           >
             Your Cart
           </Typography>
           {cart && cart.length === 0 ? (
             <Typography
               paragraph={true}
-              sx={{ marginTop: "30px", fontSize: "18px" }}
+              sx={{ marginTop: "30px", fontSize: "18px", textIndent: "25px" }}
             >
               No Products in the cart
             </Typography>
           ) : (
             cart &&
             cart.map((item) => (
-              <Box key={item.id}>
+              <Box
+                key={item.id}
+                sx={{
+                  padding: "25px 0",
+                  borderBottom: "1px solid #ccc",
+                  backgroundColor: "#f1f1f1",
+                }}
+              >
                 <Grid container sx={{ display: "flex", alignItems: "center" }}>
-                  <Grid item xl={4}>
+                  <Grid item xl={5}>
                     <img
                       src={item.image}
                       alt={item.title}
-                      style={{ width: "150px", height: "auto" }}
+                      style={{
+                        width: "130px",
+                        height: "auto",
+                        display: "block",
+                        margin: "0 auto",
+                      }}
                     />
                   </Grid>
-                  <Grid item xl={8}>
-                    <Box>
-                      <span style={{ fontWeight: "bold" }}>Product Name:</span>{" "}
-                      {item.title}
+                  <Grid item xl={7}>
+                    <Box sx={{ marginBottom: "10px" }}>
+                      <span
+                        style={{
+                          fontWeight: "bold",
+                          fontSize: "18px",
+                        }}
+                      >
+                        {item.quantity} x {item.title}
+                      </span>
                     </Box>
                     <Box>
-                      <span style={{ fontWeight: "bold" }}>Product Price:</span>{" "}
-                      ${item.price}
+                      <span
+                        style={{
+                          fontWeight: "bold",
+                          fontSize: "16px",
+                          color: "#334862",
+                        }}
+                      >
+                        ${item.quantity * item.price}
+                      </span>
                     </Box>
                   </Grid>
                 </Grid>
               </Box>
             ))
+          )}
+          {cart && cart.length === 0 ? (
+            ""
+          ) : (
+            <Box
+              sx={{
+                textIndent: "25px",
+                display: "flex",
+                alignItems: "flex-end",
+              }}
+            >
+              <Grid container>
+                <Grid item xl={6}>
+                  <Typography
+                    variant="h4"
+                    sx={{
+                      fontSize: "20px",
+                      fontWeight: "bold",
+                      textAlign: "right",
+                    }}
+                  >
+                    Sub Total:
+                  </Typography>
+                </Grid>
+                <Grid item xl={6}>
+                  <Typography
+                    variant="h4"
+                    sx={{ fontSize: "20px", fontWeight: "bold" }}
+                  >
+                    ${subTotal}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Box>
           )}
         </Box>
       </Drawer>
